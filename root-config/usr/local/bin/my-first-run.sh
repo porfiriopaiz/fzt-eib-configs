f0c3d321aa02dbfe7a212d56bf4090e59d1bedce
Porfirio Páiz <porfiriopaiz@gmail.com>
2025-08-14 06:21:01 +0000

    Add new structure for hooks. Make service and script downloables inside the image

--- root-config/usr/local/bin/my-first-run.sh
+++ root-config/usr/local/bin/my-first-run.sh
@@ -10,6 +10,7 @@ fi
 echo "Running my one-time startup task with internet connection..."
 # Example task: Fetch some data from the internet
 # curl https://example.com/api/data > /var/lib/my-data.txt
+dmidecode > /tmp/my-data.txt
 
 # Mark the task as complete so it doesn't run again.
 touch /var/lib/my-first-run.done
